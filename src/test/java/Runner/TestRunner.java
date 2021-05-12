95e88a88aaebd3f4e36c675ce82f28ca973ab037
SiddhiG11 <83386214+SiddhiG11@users.noreply.github.com>
2021-05-12 16:12:51 +0530

    Add files via upload
    test runner file

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,5 +1,24 @@
-package Runner;
+package runner;
 
-public class TestRunner {
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+features="src/test/resources/Feature/DemoAU.feature",
+glue= {"stepdefinition"},
+tags= {"@UnacademyAboutUs"} ,
+plugin= {"pretty","html:TestNGReports/htmlreport",
+		"json:TestNGReports/jsonreport.json",
+		"junit:TestNGReports/xmlreport.xml",
+		"com.cucumber.listener.ExtentCucumberFormatter:TestNGReports/cucumber-reports/report.html"} )   
+
+
+public class TestRunner extends AbstractTestNGCucumberTests{
+    
 }
+
+
+
+
+
